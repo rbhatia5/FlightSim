e28338d049b2254e3a8dcf18c8df05bdd6e1af41
Rahul Bhatia <rahul.bhatia.k@gmail.com>
2014-03-09 19:27:42 -0500

    Initial Commit

---
+++ FlightSim/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  FlightSim
+//
+//  Created by Rahul Bhatia on 3/9/14.
+//  Copyright (c) 2014 Dastardly Design. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
